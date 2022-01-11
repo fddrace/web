69096ce845fd159893b7aa380496f2b3bc141406
ChillerDragon <ChillerDragon@gmail.com>
2022-01-11 17:22:52 +0100

    Write logs to file

--- src/logger.js
+++ src/logger.js
@@ -1,8 +1,16 @@
 /* ChillerDragon's logger */
 
+const fs = require('fs')
+
 const log = (type, msg) => {
   const ts = new Date().toISOString().split('T').join(' ').split(':').join(':').split('.')[0]
-  console.log(`[${ts}][${type}] ${msg}`)
+  const logmsg = `[${ts}][${type}] ${msg}`
+  console.log(logmsg)
+  fs.appendFile('logs/logfile.txt', logmsg + '\n', (err) => {
+    if (err) {
+      throw err
+    }
+  })
 }
 
 module.exports = {
